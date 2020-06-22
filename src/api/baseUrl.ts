8270a47c87afec473f351d52559b91ecc46a3218
cat <1156549753@qq.com>
2020-06-22 20:39:57 +0800

    fix

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,11 +1,7 @@
-let env:any = process.env.NODE_ENV
-console.log(env)
+
 const baseUrl = () => {
-    switch (env) {
-        case 'production':
-            return 'https://jfms.api.dxanm.com'; //正式地址
-        case 'devlopment':
-            return 'http://jifan_test_management_system_api.jifan.net.cn'; //测试地址
+    let url:string = location.origin
+    switch (url) {
         default:
             return window.location.origin;
     }
